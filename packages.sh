e3ca38cadedbd70cc8df9000967b9a17fdef4261
Benedicta <arababenn@yahoo.co.uk>
2023-02-02 20:34:06 -0600

    missing packages

---
+++ packages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Author: Mayatu
+#Date : 02/02/2023
+##---- missing packages
+
+sudo yum install finger -y
+sudo yum install curl -y
+sudo yum install zip -y
+sudo yum install vim -y
+sudo yum install wget -y
+sudo yum install git -y
